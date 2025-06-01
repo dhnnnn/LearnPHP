6ca156ffcef1ed06e96374cba5b3ff525bdcedf6
Dhani Dwi Putra <dhanidwiputra@gmail.comgit config --global user.email dhanidwiputra371@gmail.com>
2025-06-01 07:18:17 +0700

    Lanjut opertemuan 4

---
+++ pertemuan4/date.php
@@ -0,0 +1,22 @@
+<?php
+
+    //Date
+    //Untuk menampilkan tanggal dengan format tertentu
+
+    //echo date("l, d - M - Y")
+
+
+    //time
+    //UNIX Timestamp
+    //detik yang sudah berlalu sejak 1 Januari 1970
+    //echo time();
+
+
+    //echo date("d - M - Y", time()+60*60*24*100)
+
+    //mktime
+    //membuat deteik sendiri
+    //mktime(0,0,0,0,0,0)
+    //jam, menit, detik, bulan, tanggal, tahun
+    echo date("l", mktime(0,0,0,6,27,2005));
+?>
