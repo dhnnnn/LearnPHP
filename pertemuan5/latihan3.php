e05667dede9b5ef6c344b2408edb102076f21d1c
Dhani Dwi Putra <dhanidwiputra@gmail.comgit config --global user.email dhanidwiputra371@gmail.com>
2025-06-01 20:29:16 +0700

    Menambah latihan 3

---
+++ pertemuan5/latihan3.php
@@ -0,0 +1,35 @@
+<?php
+
+$mahasiswa= [
+    ["Dhani", "09123810", "Teknik Informatika", "dhanidwi@email.com"],
+    ["Mada", "09934223810", "Teknik Informatika", "mada@email.com"]
+];
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Mahasiswa</title>
+</head>
+<body>
+    
+    <h1>Daftar Mahasiswa</h1>
+
+<?php foreach($mahasiswa as $maha) : ?>
+
+    <ul>
+        <li>Nama : <?= $maha[0]; ?></li>
+        <li>NPM : <?= $maha[1]; ?></li>
+        <li>Jurusan : <?= $maha[2]; ?></li>
+        <li>Email : <?= $maha[3]; ?></li>
+    </ul>
+        
+<?php endforeach; ?>
+
+</body>
+</html>
